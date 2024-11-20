277a22561cb0966e1a0da0ae98e23c6a125976fc
Brandon Beidel <bcbeidel@gmail.com>
2024-11-19 23:13:12 -0500

    break up writing crew into multi agent

--- src/five_level_explainer/crews/writing_crew/writing_crew.py
+++ src/five_level_explainer/crews/writing_crew/writing_crew.py
@@ -3,32 +3,110 @@ from pydantic import BaseModel
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 
-class DraftPost(BaseModel):
-    title: str
-    content: str
-
 @CrewBase
 class WritingCrew():
-	"""fivelevels crew"""
+	"""writing crew"""
 
 	agents_config = 'config/agents.yaml'
 	tasks_config = 'config/tasks.yaml'
 
-
 	@agent
-	def writer(self) -> Agent:
+	def lead_writer(self) -> Agent:
 		return Agent(
-			config=self.agents_config['writer'],
+			config=self.agents_config['lead_writer'],
 			verbose=False,
-			tools=[],
+			llm='gpt-4o-mini'
+		)
+	
+	@agent
+	def childrens_writer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['childrens_writer'],
+			verbose=False,
+			llm='gpt-4o-mini'
+		)
+	
+	@agent
+	def teen_writer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['teen_writer'],
+			verbose=False,
+			llm='gpt-4o-mini'
+		)
+	
+	@agent
+	def college_writer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['college_writer'],
+			verbose=False,
 			llm='gpt-4o-mini'
 		)
 
+	@agent
+	def college_writer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['college_writer'],
+			verbose=False,
+			llm='gpt-4o-mini'
+		)
+
+	@agent
+	def graduate_writer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['graduate_writer'],
+			verbose=False,
+			llm='gpt-4o-mini'
+		)
+
+	@agent
+	def postgrad_writer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['postgrad_writer'],
+			verbose=False,
+			llm='gpt-4o-mini'
+		)
+	
 	@task
-	def draft_content_task(self) -> Task:
+	def draft_childrens_content_task(self) -> Task:
 		return Task(
-			config=self.tasks_config['draft_content_task'],
-			output_pydantic=DraftPost
+			config=self.tasks_config['draft_childrens_content_task'],
+		)
+
+	@task
+	def draft_teen_content_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['draft_teen_content_task'],
+		)
+	
+	@task
+	def draft_undergrad_content_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['draft_undergrad_content_task'],
+		)
+	
+	@task
+	def draft_graduate_content_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['draft_graduate_content_task'],
+		)
+	
+	@task
+	def draft_postgrad_content_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['draft_postgrad_content_task'],
+		)
+
+	@task
+	def combine_draft_content_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['combine_draft_content_task'],
+			context=[
+				self.draft_childrens_content_task(),
+				self.draft_teen_content_task(),
+				self.draft_undergrad_content_task(),
+				self.draft_graduate_content_task(),
+				self.draft_postgrad_content_task(),
+			]
 		)
 
 	@crew
@@ -37,7 +115,7 @@ class WritingCrew():
 		return Crew(
 			agents=self.agents, # Automatically created by the @agent decorator
 			tasks=self.tasks, # Automatically created by the @task decorator
-			process=Process.sequential,
 			verbose=True,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+			process=Process.sequential, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+    		planning=True
 		)
